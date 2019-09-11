ca045e4e646c274d7d1b49f2c0965c9ca15d5711
Edward Ballesteros <eballesteros@usautoparts.com>
2019-09-11 12:55:00 +0800

    Added Grommet, Styled Components and React Router.

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import FormContainer from "./js/components/container/FormContainer.jsx";
+import SampleRouter from "./js/components/container/SampleRouter";
+
+
+
+ReactDOM.render(
+    <SampleRouter />,
+    document.getElementById('route')
+);
